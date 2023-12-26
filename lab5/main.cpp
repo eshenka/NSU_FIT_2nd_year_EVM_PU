3f46ac0853911b26fb29629a8209900755ae1c4e
Yulia <yuliaolegyureva@gmail.com>
2023-12-26 19:23:04 +0700

    added lab5

---
+++ lab5/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    VideoCapture capture(0);
+}
